9223853c4375919b172ea08c9f722558a9c3714e
Miran Mamsaleh <m.mamsaleh@students.htl-leonding.ac.at>
2018-01-10 16:49:12 +0100

    Created Project with Service Worker

--- chatClient/src/app/app.module.ts
+++ chatClient/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
